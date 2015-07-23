f66ad6f7a4eaeff3779063cf60ea32e5902a6586
Pieter van den Berg <pvdberg@gmail.com>
2015-07-23 00:32:34 -0300

    update install_rstan.R

--- install_rstan.R
+++ install_rstan.R
@@ -3,3 +3,10 @@ if (!dir.exists(dotR)) dir.create(dotR)
 M <- file.path(dotR, "Makevars")
 if (!file.exists(M)) file.create(M)
 cat("\nCXXFLAGS=-O3", file = M, sep = "\n", append = TRUE)
+cat("\nCXXFLAGS+=-flto -ffat-lto-objects", 
+    file = M, sep = "\n", append = TRUE)
+
+
+Sys.setenv(MAKEFLAGS = "-j4")
+
+install.packages("rstan", dependencies = TRUE)
